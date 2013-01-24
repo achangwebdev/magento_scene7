d42c35f125e22bac0ee005de94f5e11e7c4fba9a
Minglong <liminglong2008@gmail>
2013-01-24 12:43:20 +0800

    js

---
+++ js/jquery/jquery-noconflict.js
@@ -0,0 +1,29 @@
+var $j = jQuery.noConflict();
+
+function obj2str(o) {
+	var r = [];
+	if (typeof o == "string" || o == null) {
+		return o;
+	}
+	if (typeof o == "object") {
+		if (!o.sort) {
+			r[0] = "{";
+			for ( var i in o) {
+				r[r.length] = i;
+				r[r.length] = ":";
+				r[r.length] = obj2str(o[i]);
+				r[r.length] = ",";
+			}
+			r[r.length - 1] = "}";
+		} else {
+			r[0] = "[";
+			for ( var i = 0; i < o.length; i++) {
+				r[r.length] = obj2str(o[i]);
+				r[r.length] = ",";
+			}
+			r[r.length - 1] = "]";
+		}
+		return r.join("");
+	}
+	return o.toString();
+}
